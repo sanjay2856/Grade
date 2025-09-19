3676c6fbc45bf01ebe06739c47ee38251f3a0511
sanjay2856 <sanjay_259138@saitm.ac.in>
2025-09-19 10:22:47 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,10 @@
+marks=int(input("your marks:"))
+if marks <=25:
+    print("your grade is D")
+elif marks >25 and marks <=50:
+    print("your grade is c")
+elif marks >50 and marks <=75:
+    print ("your grade is B")
+elif marks >75 and marks <=100:
+    print("your grade is A")
+
